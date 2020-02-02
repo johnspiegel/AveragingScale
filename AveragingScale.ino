edf19b98bb8cafa86692f275a1f782d3b098a68c
John Spiegel <johnbspiegel@gmail.com>
2020-02-01 17:12:03 -0800

    New display format, and smoothing.

--- AveragingScale.ino
+++ AveragingScale.ino
@@ -38,6 +38,10 @@ constexpr double GRAMS_PER_RAW = 363.79 / 135703.0;
 constexpr double GRAMS_PER_RAW = 0.0009570014631347681;
 #endif
 
+inline long tenths(double f) {
+  return round(f*10);
+}
+
 char* float_to_str(float f) {
   static char fbuf[16];
   // 7-wide: handle negative thousands with one decimal point:
@@ -54,7 +58,8 @@ char* float_to_str(float f) {
   return dtostrf(f, 7, 1, fbuf);
 #else
   float absf = abs(f);
-  snprintf(fbuf, sizeof(fbuf), "%5d.%1d", int(absf), int((absf - int(absf))*10));
+  long t = tenths(absf);
+  snprintf(fbuf, sizeof(fbuf), "%5d.%1d", t/10, t % 10);
   if (f < 0) {
     int i = 3;
     for (i = 3; i >= 0 && fbuf[i] != ' '; i--) {}
@@ -70,10 +75,13 @@ class AveragingScale {
     bool wiredBackwards_;
     int32_t zero_;
     double unitsPerRaw_;
+#if defined(__AVR__)
     CircularBuffer<int32_t, 50> buf_;
+#else
+    CircularBuffer<int32_t, 100> buf_;
+#endif
 
     bool averaging_;
-    bool autoAveraging_;
     int32_t averagingSum_;
     int averagingSampleCount_;
   
@@ -83,8 +91,7 @@ class AveragingScale {
       wiredBackwards_(wiredBackwards),
       zero_(0),
       unitsPerRaw_(unitsPerRaw),
-      averaging_(false),
-      autoAveraging_(false)
+      averaging_(false)
       {}
 
     int32_t readRaw() {
@@ -93,14 +100,14 @@ class AveragingScale {
         v = -v;
       }
       buf_.push(v);
-      if (averaging_ || autoAveraging_) {
+      if (averaging_) {
         averagingSum_ += v;
         averagingSampleCount_++;
       }
       return v;
     }
 
-    struct AverageStats {
+    struct RawAverageStats {
       int32_t minValue;
       int32_t maxValue;
       int32_t sum;
@@ -108,12 +115,12 @@ class AveragingScale {
       int32_t average;
     };
 
-    double getAverageUnits(byte sampleCount) {
+    double getAverageUnits(byte sampleCount) const {
       return rawToUnits(getRawAverage(sampleCount).average);
     }
 
-    AverageStats getRawAverage(byte sampleCount) {
-      AverageStats stats;
+    RawAverageStats getRawAverage(byte sampleCount) const {
+      RawAverageStats stats;
       if (buf_.size() == 0) {
         return stats;
       }
@@ -146,23 +153,38 @@ class AveragingScale {
       while (buf_.size() < sampleCount) {
         readRaw();
       }
-      AverageStats stats = getRawAverage(sampleCount);
+      RawAverageStats rawAvg = getRawAverage(sampleCount);
       zero_ = getRawAverage(sampleCount).average;
       Serial.print(F("Zero!  zero_: "));
       Serial.print(zero_);
       Serial.print(F(" "));
-      Serial.print(stats.sum);
+      Serial.print(rawAvg.sum);
       Serial.print(F(" "));
-      Serial.print(stats.sampleCount);
+      Serial.print(rawAvg.sampleCount);
       Serial.print(F(" "));
       Serial.print(buf_.size());
       Serial.println();
     }
 
     int32_t getRaw() const {
-      if (averaging_ || autoAveraging_) {
+      if (averaging_) {
         return averagingSum_ / averagingSampleCount_;
       } else {
+        // Smooth a it a bit over the last 200-300ms.
+        double instantUnits = getInstantUnits();
+        for (int seconds = 5; seconds > 0; seconds--) {
+          long rawAverage = getRawAverage(/*sampleCount=*/ seconds * 10).average;
+          if (abs(rawToUnits(rawAverage) - instantUnits) < (0.3/seconds)) {
+            Serial.print(F("Using "));
+            Serial.print(seconds);
+            Serial.print(F("s average: "));
+            Serial.print(rawToUnits(rawAverage));
+            Serial.print(F(" - instant: "));
+            Serial.print(instantUnits);
+            Serial.println();
+            return rawAverage;
+          }
+        }
         return buf_[0];
       }
     }
@@ -189,14 +211,12 @@ class AveragingScale {
 
     void startAveraging() {
       averaging_ = true;
-      autoAveraging_ = false;
       averagingSum_ = buf_[0];
       averagingSampleCount_= 1;
     }
 
     void stopAveraging() {
       averaging_ = false;
-      autoAveraging_ = false;
     }
 
     double mostStableRecentAverage() {
@@ -225,26 +245,22 @@ class AveragingScale {
       return averaging_;
     }
 
-    bool isAutoAveraging() const {
-      return autoAveraging_;
-    }
-
     void trackZeroDrift() {
       if (averaging_) {
         return;
       }
 
       const byte sampleCount = 20;  // 2s at 10 samples-per-second.
-      AverageStats stats = getRawAverage(sampleCount);
+      RawAverageStats rawAvg = getRawAverage(sampleCount);
 
       // Don't try to track drift if zero isn't stable.
-      if ((stats.maxValue - stats.minValue) * unitsPerRaw_ > 1.0) {
+      if ((rawAvg.maxValue - rawAvg.minValue) * unitsPerRaw_ > 1.0) {
         return;
       }
 
       // Don't track drift if the change is too large.
       // And don't make lots of zero adjustments for small changes either.
-      float diffUnits = abs((stats.average - zero_) * unitsPerRaw_);
+      float diffUnits = abs((rawAvg.average - zero_) * unitsPerRaw_);
       if (diffUnits > 1.0 || diffUnits < 0.3) {
           return;
       }
@@ -254,69 +270,11 @@ class AveragingScale {
       Serial.print(F(" oldzero: "));
       Serial.print(zero_);
       Serial.print(F(" newzero: "));
-      Serial.print(stats.average);
+      Serial.print(rawAvg.average);
       Serial.print(F(" diffUnits: "));
       Serial.print(diffUnits);
       Serial.println();
-      zero_ = stats.average;
-    }
-
-    bool autoAverage() {
-      if (averaging_ || autoAveraging_) {
-        return false;
-      }
-
-      const byte sampleCount = 5;
-      AverageStats stats = getRawAverage(sampleCount);
-
-      if (stats.maxValue > 1.05 * stats.average) {
-        return false;
-      }
-      if (stats.minValue < 0.95 * stats.average) {
-        return false;
-      }
-      if ((stats.average -zero_) * unitsPerRaw_ < 1.0) {
-        return false;
-      }
-
-      autoAveraging_ = true;
-      averagingSum_ = stats.sum;
-      averagingSampleCount_= stats.sampleCount;
-      Serial.print(F("Auto-Average! "));
-      Serial.println(getUnits());
-      return true;
-    }
-
-    bool autoAverageOff() {
-      if (averaging_ || !autoAveraging_) {
-        return false;
-      }
-
-      const byte sampleCount = 5;
-      AverageStats stats = getRawAverage(sampleCount);
-
-      int32_t currentAverage = getRaw();
-      /*
-      if (stats.maxValue < 1.1 * currentAverage &&
-          stats.minValue > 0.9 * currentAverage) {
-        return false;
-      }
-      */
-      if (stats.average < 1.1 * currentAverage &&
-          stats.average > 0.9 * currentAverage) {
-        return false;
-      }
-
-      /*
-      float diffUnits = (stats.average - zero_) * unitsPerRaw_;
-      if (diffUnits > 1.0) {
-        return false;
-      }
-      */
-
-      Serial.println(F("Auto-Average Off! "));
-      stopAveraging();
-      return true;
+      zero_ = rawAvg.average;
     }
 };
 
@@ -332,13 +290,6 @@ void wakeUp() {
   Serial.print(F("POWER PIN: "));
   Serial.print(digitalRead(POWER_BUTTON_PIN));
   Serial.println();
-
-  // setup();
-  /*
-  watchDigitalPin(POWER_BUTTON_PIN);
-  display.begin();
-  hx711.power_up();
-  */
 }
 
 void goToSleep() {
@@ -366,6 +317,7 @@ void goToSleep() {
   Serial.println(F("Yawn.. done sleeping?!"));
 }
 
+
 void setup() {
   Serial.begin(9600);
   Serial.println(F("-------------------------"));
@@ -382,11 +334,10 @@ void setup() {
     display.setCursor(0, 16+46);
     display.println(F("  scale"));
   } while( display.nextPage() );
-  long splashStart = millis();
+  long splashStartMillis = millis();
 
   Serial.println(F("Starting scale..."));
   hx711.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
-  ascale.zero(1);
   Serial.println(F("Scale started"));
 
   Serial.println(F("Watching button pins..."));
@@ -395,100 +346,132 @@ void setup() {
   watchDigitalPin(AVG_BUTTON_PIN);
   watchDigitalPin(COFFEE_BUTTON_PIN);
 
-  long delayTime = splashStart + 2000 - millis();
-  delay(delayTime);
-  Serial.print(F("Leaving splash up for: "));
-  Serial.print(delayTime);
+  // Prime the scale and then zero while splash screen is up.
+  long delayUntilMillis = splashStartMillis + 1000;
+  while (millis() < delayUntilMillis) {
+    ascale.readRaw();
+  }
+  ascale.zero(/*sampleCount=*/ 5);
+
+  Serial.print(F("Splash start time: "));
+  Serial.print(splashStartMillis);
   Serial.println(F("ms"));
-  Serial.println(F("Setup complete."));
+
+  Serial.println(F("Setup complete. time: "));
+  Serial.print(millis());
+  Serial.println(F("ms"));
   Serial.println(F("-------------------------"));
 }
 
-void displayAveraging(U8G2 *u8g2,
+void newDisplay( U8G2 *u8g2,
                       double grams,
                       bool averaging,
                       double instantGrams,
                       const CircularBuffer<double, 2> holds) {
-  // Current value big at top
-  u8g2->setFont(u8g2_font_profont22_tr);
-  u8g2->setCursor(0, 14);
+  char buf[16] = {0};
+  // Hold at the top.
+  if (holds.size()) {
+    u8g2->setFont(u8g2_font_profont22_tr);
+    snprintf(buf, sizeof(buf), "%sg", float_to_str(holds[0]));
+    u8g2->setCursor(128 - u8g2->getStrWidth(buf), 23);
+    u8g2->print(buf);
+    u8g2->setCursor(0, 23);
+    u8g2->print("H");
+  }
+
+  // Current value big at bottom
+  u8g2->setFont(u8g2_font_profont29_tr);
+  snprintf(buf, sizeof(buf), "%sg", float_to_str(grams));
+  u8g2->setCursor(u8g2->getDisplayWidth() - u8g2->getStrWidth(buf),
+                  u8g2->getDisplayHeight() + u8g2->getDescent());
+  u8g2->print(static_cast<char*>(buf));
   if (ascale.isAveraging()) {
-    u8g2->print("AVG");
-  } else if (ascale.isAutoAveraging()) {
-    u8g2->print("AUT");
-  } else {
-    u8g2->print("   ");
+    u8g2->setFont(u8g2_font_profont29_tr);
+    // Make an "x-bar" by drawing a line across the top of an "x".
+    u8g2->setCursor(0, u8g2->getDisplayHeight() + u8g2->getDescent());
+    u8g2->print("x");
+    u8g2->drawBox(0, u8g2->getDisplayHeight() + u8g2->getDescent() - 19,
+                  14, 3);
   }
-  u8g2->setCursor(46, 14);
-  u8g2->print(float_to_str(grams));
-
-  // Any hold values
-  u8g2->setFont(u8g2_font_profont17_tr);
-  for (int holdIndex = 0; holdIndex < 2 && holdIndex < holds.size(); holdIndex++) {
-    u8g2->setCursor(0, 17 * (holdIndex+2));
-    u8g2->print("HLD");
-    u8g2->setCursor(65, 17 * (holdIndex+2));
-    u8g2->print(float_to_str(holds[holdIndex]));
-  }
-
-  // Instant value
-  u8g2->setFont(u8g2_font_profont12_tr);
-  u8g2->setCursor(0, 64);
-  u8g2->print("Instant:");
-  u8g2->setCursor(86, 64);
-  u8g2->print(float_to_str(ascale.getInstantUnits()));
 }
 
-void displayCoffee(U8G2 *u8g2, double grams, long coffeeTime) {
+void displayCoffee(U8G2 *u8g2, double grams, bool averaging, long coffeeTime) {
   char buf[16];
-  // Current value big at top
+  // Time at top
   u8g2->setFont(u8g2_font_profont29_tr);
   u8g2->setCursor(0, 23);
   int minutes = (coffeeTime / 1000) / 60;
   int seconds = (coffeeTime / 1000) % 60;
   snprintf(buf, sizeof(buf), "  %2d:%02ds", minutes, seconds);
+  u8g2->setCursor(u8g2->getDisplayWidth() - u8g2->getStrWidth(buf),
+                  23);
   u8g2->print(buf);
 
+  // Current value big at bottom
   u8g2->setFont(u8g2_font_profont29_tr);
-  u8g2->setCursor(0, 58);
   snprintf(buf, sizeof(buf), "%sg", float_to_str(grams));
+  u8g2->setCursor(u8g2->getDisplayWidth() - u8g2->getStrWidth(buf),
+                  u8g2->getDisplayHeight() + u8g2->getDescent());
   u8g2->print(static_cast<char*>(buf));
-  /*
-  u8g2->setCursor(0, 19);
   if (ascale.isAveraging()) {
-    u8g2->print("AVG");
-  } else if (ascale.isAutoAveraging()) {
-    u8g2->print("AUT");
-  } else {
-    u8g2->print("   ");
+    u8g2->setFont(u8g2_font_profont29_tr);
+    // Make an "x-bar" by drawing a line across the top of an "x".
+    u8g2->setCursor(0, u8g2->getDisplayHeight() + u8g2->getDescent());
+    u8g2->print("x");
+    u8g2->drawBox(0, u8g2->getDisplayHeight() + u8g2->getDescent() - 19,
+                  14, 3);
   }
-  */
 }
 
 void loop() {
-  display.clear();
   CircularBuffer<double, 2> holds;
   bool coffeeMode = false;
   long coffeeStart = 0;
+  int smoothCount = 0;
+  double prevValue = ascale.getUnits();
 
   long lastLoop = millis();
   while (true) {
     long readStart = millis();
     ascale.readRaw();
     long readDone = millis();
-    /*
     Serial.print(F("Time to complete previous loop: "));
     Serial.print(readStart - lastLoop);
     Serial.print(" : ");
     Serial.print(F("  Time to readRaw: "));
     Serial.print(readDone - readStart);
     Serial.println();
+    /*
     */
     lastLoop = readStart;
 
-    double value = ascale.getUnits();
-    // Don't distract with tiny fluctuations around zero.
-    if (!ascale.isAveraging() && value < 0.3 && value > -0.3) {
+    double rawValue = ascale.getRaw();
+    // double value = ascale.getUnits();
+    double value = ascale.rawToUnits(rawValue);
+
+    // Don't distract with tiny fluctuations.
+    if (abs(value - prevValue) < 0.1 &&
+        abs(tenths(value) - tenths(prevValue)) == 1
+        /* && smoothCount < 5 */ ) {
+      Serial.print(F("Using smoothed value. SmoothCount: "));
+      Serial.print(smoothCount);
+      Serial.print(F(" value: "));
+      Serial.print(value);
+      Serial.print(F(" tenths: "));
+      Serial.print(tenths(value));
+      Serial.print(F(" prevValue: "));
+      Serial.print(prevValue);
+      Serial.print(F(" tenths: "));
+      Serial.print(tenths(prevValue));
+      Serial.println();
+      value = prevValue;
+      smoothCount++;
+    } else {
+      prevValue = value;
+      smoothCount = 0;
+    }
+    // Don't show -0.0.
+    if (abs(value) < 0.5) {
       value = 0.0;
     }
 
@@ -501,9 +484,9 @@ void loop() {
     display.firstPage();
     do {
       if (coffeeMode) {
-        displayCoffee(&display, value, coffeeStart ? millis() - coffeeStart : 0 );
+        displayCoffee(&display, value, ascale.isAveraging(), coffeeStart ? millis() - coffeeStart : 0);
       } else {
-        displayAveraging(&display, value, ascale.isAveraging(), ascale.getInstantUnits(), holds);
+        newDisplay(&display, value, ascale.isAveraging(), ascale.getInstantUnits(), holds);
       }
     } while ( display.nextPage() );
 
@@ -524,7 +507,6 @@ void loop() {
       Serial.print(F("ZERO was pressed"));
       Serial.println();
       ascale.setZero(ascale.getRaw());
-      // goToSleep();
     } else {
       ascale.trackZeroDrift();
     }
@@ -552,8 +534,8 @@ void loop() {
       Serial.println(avg.sampleCount);
 
       double recentAverage = ascale.getAverageUnits(10);
-      if (value > 10.0 && abs(recentAverage) < 1.0 ||
-        value <= 10.0 && abs(recentAverage / value) < 0.1) {
+      if ((value > 10.0 && abs(recentAverage) < 1.0) ||
+          (value > 1.0 && value <= 10.0 && abs(recentAverage / value) < 0.1)) {
         double holdValue = ascale.mostStableRecentAverage();
         holds.push(holdValue);
         Serial.println("STOP averaging: ");
@@ -573,7 +555,8 @@ void loop() {
         coffeeStart = 0;
         Serial.print(F("Zeroing..."));
         Serial.println();
-        ascale.setZero(ascale.getRaw());
+        ascale.setZero(rawValue);
+        ascale.zero(/*sampleCount=*/ 5);
         Serial.print(F("Coffee Mode on!"));
         Serial.println();
       }
